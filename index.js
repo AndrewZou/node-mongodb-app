cc62bc90df28ee11a94e5d7e938e1e22e5cbee53
AndrewZou <zou.andrew@gmail.com>
2020-07-24 10:50:19 -0500

    Replace Callback method with Promise chain

--- index.js
+++ index.js
@@ -5,37 +5,36 @@ const dbaccessor = require('./dbaccessor');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
-mongoClient.connect( url, ( err, client ) =>{
-    assert.equal( err, null );
-
+mongoClient.connect( url)
+.then( client  =>{
     console.log('Connected successfully to MongoDB server!');;
 
     const db = client.db( dbname );
     const collection = db.collection("dishes");
 
-    dbaccessor.insertDocument(db, { name: "Vadonut", description: "Test"},
-        "dishes", (result) => {
-            console.log("Insert Document:\n", result.ops);
+    dbaccessor.insertDocument(db, { name: "Vadonut", description: "Test"}, "dishes")
+    .then( result => {
+        console.log("Insert Document:\n", result.ops);
 
-            dbaccessor.findDocuments(db, "dishes", (docs) => {
-                console.log("Found Documents:\n", docs);
-
-                dbaccessor.updateDocument(db, { name: "Vadonut" },
-                    { description: "Updated Test" }, "dishes",
-                    (result) => {
-                        console.log("Updated Document:\n", result.result);
-
-                        dbaccessor.findDocuments(db, "dishes", (docs) => {
-                            console.log("Found Updated Documents:\n", docs);
-                            
-                            db.dropCollection("dishes", (result) => {
-                                console.log("Dropped Collection: ", result);
-
-                                client.close();
-                            });
-                        });
-                    });
-            });
-    });
-
-});
+        return dbaccessor.findDocuments(db, "dishes");
+    })
+    .then( docs => {
+        console.log("Found Documents:\n", docs);
+        return dbaccessor.updateDocument(db, { name: "Vadonut" },
+                    { description: "Updated Test" }, "dishes");
+    })
+    .then( result => {
+        console.log("Updated Document:\n", result.result);
+        return dbaccessor.findDocuments(db, "dishes");
+    })
+    .then( docs => {
+        console.log("Found Updated Documents:\n", docs);                            
+        return db.dropCollection("dishes");
+    })
+    .then( result => {
+        console.log("Dropped Collection: ", result);
+        return client.close();
+    })
+    .catch( err => console.log(err));
+}).
+catch( err => console.log(err));
